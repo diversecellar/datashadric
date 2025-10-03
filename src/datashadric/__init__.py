af106e912311687cd85d7dd92568303a3665b18c
diversecellar <kabwenzenamalomba@gmail.com>
2025-10-03 08:36:30 +0200

    Supplemental release, Additional functions for outlier detection, Additional functions for plotting (LOWESS meanline plotter), Additional functions for data clustering based on k-means

--- src/datashadric/__init__.py
+++ src/datashadric/__init__.py
@@ -23,7 +23,7 @@ Basic Usage:
 >>> from datashadric.plotters import df_boxplotter
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Paul Namlaomba (GitHub: diversecellar)"
 __email__ = "kabwenzenamalomba@gmail.com"
 
